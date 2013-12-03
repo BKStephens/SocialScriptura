d5e63b1c774fcaf144de1b839dc4db758341288b
Ben Stephens <BStephens@daxko.com>
2013-12-03 10:47:06 -0600

    Added welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SocialScriptura::Application.routes.draw do
+  root :to => 'welcome#index'
+  #get "welcome/index"
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
