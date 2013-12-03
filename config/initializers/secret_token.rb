cf4ac04072d1ed21266d18b52a0684e0643f302e
Ben Stephens <BStephens@daxko.com>
2013-12-02 20:50:46 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SocialScriptura::Application.config.secret_key_base = 'c0fea6c629f6fe87c82af122d5d3e2a0c8ca7ed924aa9cbbdd525d65c1af8432f44996c34177145175d5411318806324aee83e2c0f52e76e346039a2a877f50f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SocialScriptura::Application.config.session_store :cookie_store, key: '_SocialScriptura_session'
