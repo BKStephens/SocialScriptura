69e04d8512b464bc4f7fc636193dfe029f31c34c
Ben Stephens <BStephens@daxko.com>
2013-12-03 10:32:27 -0600

    Initial setup for  Devise

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+
+  #This is not needed in Rails 4
+  # attr_accessible :email, :password, :password_confirmation, :remember_me, :church, :denomination, :user_name, :full_name
+end
