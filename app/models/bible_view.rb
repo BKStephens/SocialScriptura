09baa001b748f7abd95449f7796ffd46d65a3fa8
Ben Stephens <BStephens@daxko.com>
2014-03-08 14:40:51 -0600

    Got the dropdown lists to work for bible version and chapter

--- app/models/bible_view.rb
+++ app/models/bible_view.rb
@@ -6,7 +6,7 @@ class BibleView
   attr_accessor :bible_version, :book, :chapter, :verse_from, :verse_to
 
   def initialize(params = {})
-    self.bible_version = params[bible_version] || 'asv.xml'
+    self.bible_version = params[:bible_version] || 'kjv.xml'
     self.book = params[:book] || 'Genesis'
     self.chapter = params[:chapter] || '1'
     self.verse_from = params[:verse_from]
