62be7779c55b4927da460f3e6daf6f8a9aea5f22
Ben Stephens <BStephens@daxko.com>
2014-03-18 20:47:51 -0500

    Moved defaut values for bible view from controller to model

--- app/models/bible_view.rb
+++ app/models/bible_view.rb
@@ -7,13 +7,20 @@ class BibleView
 
   def initialize(params = {})
     temp_chapter = BibleChapters.where(:id => params[:chapter]).select("chapters, bible_books_id").first
-    temp_book = BibleBooks.where(:id => temp_chapter[:bible_books_id]).select("book").first
-
-    self.bible_version = params[:bible_version] || 'kjv.xml'
-    self.book = temp_book[:book]
-    self.chapter = temp_chapter[:chapters]
-    self.verse_from = params[:verse_from]
-    self.verse_to = params[:verse_to]
+    
+    unless temp_chapter.nil? then
+      self.bible_version = params[:bible_version]
+      self.book = BibleBooks.where(:id => temp_chapter[:bible_books_id]).select("book").first[:book]
+      self.chapter = temp_chapter[:chapters]
+      self.verse_from = params[:verse_from]
+      self.verse_to = params[:verse_to]
+    else
+      bible_book = BibleBooks.where(:book => 'Genesis').select("id, book").first
+      bible_chapter = BibleChapters.where(:chapters => '1', :bible_books_id => bible_book).select("id, chapters").first
+      self.bible_version = 'asv.xml'
+      self.book = bible_book[:book]
+      self.chapter = bible_chapter[:chapters]
+    end
   end
 
   def persisted?
