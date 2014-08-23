0e9e06321e82582b11a407a0731788608a4ced30
Ben Stephens <BStephens@daxko.com>
2014-08-23 16:32:57 -0500

    Fixed #7 so that forgot password emails will be sent.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,21 @@ SocialScriptura::Application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  
+  # ActionMailer Config
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'socialscriptura.com' }
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["EMAIL_DOMAIN"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["EMAIL_USERNAME"],
+    password: ENV["EMAIL_PASSWORD"]
+  }
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
